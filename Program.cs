bca65a2e55b76598f5de656c9eebdd5973d73a15
O'Brien <44230356+OBrienZimba@users.noreply.github.com>
2024-11-13 14:12:53 +0200

    modified password in app settings and the IP in program.cs

--- Program.cs
+++ Program.cs
@@ -30,7 +30,7 @@ builder.Services.AddSwaggerGen();
 builder.WebHost.ConfigureKestrel(options =>
 {
     // Listen on localhost and the specified IP for consistency on port 7137
-    options.Listen(IPAddress.Parse("192.168.148.82"), 7137, listenOptions => listenOptions.UseHttps()); // This is HTTPS for your IP
+    options.Listen(IPAddress.Parse("192.168.**.**"), 7137, listenOptions => listenOptions.UseHttps()); // This is HTTPS for your IP
     options.Listen(IPAddress.Loopback, 7137, listenOptions => listenOptions.UseHttps()); // This is HTTPS for localhost
 });
 
